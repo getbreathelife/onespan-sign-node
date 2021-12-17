81406863b2aad179fc5624b9b36ce9a10ba7a013
SC Haw <haw@breathelife.com>
2021-12-17 10:57:17 -0500

    Added error class TODO

--- src/api/error.ts
+++ src/api/error.ts
@@ -1,5 +1,6 @@
 import { Response } from 'node-fetch';
 
+// TODO: Set `code`, `messageKey` and `messages` from OneSpan's error response as public class property
 export class HttpResponseError extends Error {
   constructor(public response: Response) {
     super(`HTTP Error Response: ${response.status} ${response.statusText}`);
