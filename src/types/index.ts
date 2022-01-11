c2fd5f747da5b9bcc4f50ca21e53c7f98ffe99cd
SC Haw <haw@breathelife.com>
2022-01-11 09:44:44 -0500

    Fix namespaced exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 // Entity typing reference: https://community.onespan.com/sites/default/files/interactive_api_references/oss_1145.yaml
+import * as Requests from './requests';
+import * as Responses from './responses';
 
 export * from './models';
-export * as Requests from './requests';
-export * as Responses from './responses';
 export * from './utils';
+export { Requests, Responses };
