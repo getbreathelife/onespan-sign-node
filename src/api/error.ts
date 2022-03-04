0cf51c11c8e05c760eca00ab514c1caf7f7f9d34
Seng Chiat Haw <haw@breathelife.com>
2022-03-04 15:38:24 -0500

    Improved error handling (#24)
    * Added `ClientError` class and `OneSpanResponseError` class
    
    * Added tests for error handling
    
    * Generated API report
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/api/error.ts
+++ src/api/error.ts
@@ -1,8 +1,45 @@
-import { Response } from 'node-fetch';
+/**
+ * Error thrown when there's an error on the client.
+ * For example, operational errors or aborted requests.
+ *
+ * @public
+ */
+export class ClientError extends Error {
+  public readonly name = 'ClientError';
+  public readonly isAborted: boolean;
+  public readonly originalError: Error;
 
-// TODO - #4: Set `code`, `messageKey` and `messages` from OneSpan's error response as public class property
-export class HttpResponseError extends Error {
-  constructor(public response: Response) {
-    super(`HTTP Error Response: ${response.status} ${response.statusText}`);
+  constructor(error: Error) {
+    super('An error occurred on the client.');
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ClientError);
+    }
+
+    this.isAborted = error.name === 'AbortError';
+    this.originalError = error;
+  }
+}
+
+/**
+ * Error thrown when there's an unsuccessful response from OneSpan Sign's API.
+ * More specifically, API responses with status code &gt;= 200 && &lt; 300.
+ *
+ * @public
+ */
+export class OneSpanResponseError extends Error {
+  public readonly name = 'OneSpanResponseError';
+  public readonly code?: number;
+  public readonly messageKey?: string;
+
+  constructor(response: Record<string, any>, public readonly statusCode: number) {
+    super(response.message ?? 'An unsuccessful response has been received from the API call.');
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, OneSpanResponseError);
+    }
+
+    this.code = response.code;
+    this.messageKey = response.messageKey;
   }
 }
