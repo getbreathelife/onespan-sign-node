4411917d2c427ad14327d30a3c759be8f41f0d8d
SC Haw <haw@breathelife.com>
2022-03-11 13:41:58 -0500

    Typed account interface

--- src/types/models/account.ts
+++ src/types/models/account.ts
@@ -1,2 +1,32 @@
+import { Nullable } from '../utils';
+import { DateTimeString, TimeZoneId } from './shared';
+
+export interface CustomField {}
+
+export interface AccountProvider {}
+
+export interface License {}
+
+export interface Company {}
+
 /** @public */
-export interface Account {}
+export interface Account {
+  id: string;
+  name: string;
+  owner: string;
+  customFields: CustomField[];
+  timezoneId: TimeZoneId;
+  licenses: License[];
+  company: Company;
+
+  providers: Nullable<AccountProvider[]>;
+  data: Nullable<Record<string, any>>;
+  logoUrl: Nullable<string>;
+  logoAltTextKey: Nullable<string>;
+
+  /** Date and time when the account is created. */
+  created: DateTimeString;
+
+  /** Date and time when the account is updated. */
+  updated: DateTimeString;
+}
--- src/types/models/sender.ts
+++ src/types/models/sender.ts
@@ -3,6 +3,7 @@ import { Account } from './account';
 import { Address } from './address';
 import { External } from './external';
 import { GroupMembership } from './group';
+import { DateTimeString, TimeZoneId } from './shared';
 import { SignatureStyle } from './signature';
 import { ProfessionalIdentityField, SpecialUserType, UserCustomField } from './user';
 
@@ -25,17 +26,7 @@ export interface Sender {
   address: Nullable<Address>;
   language: Nullable<string>;
   data: Nullable<Record<string, any>>;
-
-  /**
-   * Timezone IDs as defined in the {@link https://www.iana.org/time-zones | Time Zone Database}.
-   *
-   * @remarks
-   * - {@link https://community.onespan.com/documentation/onespan-sign/guides/feature-guides/developer/configuring-time-zones | Configuring time zones}
-   *
-   * - {@link https://en.wikipedia.org/wiki/List_of_tz_database_time_zones | List of tz database time zones (Wikipedia)}
-   */
-  timezoneId: string;
-
+  timezoneId: TimeZoneId;
   external: Nullable<External>;
   company: string;
   phone: Nullable<string>;
@@ -49,35 +40,15 @@ export interface Sender {
   specialTypes: SpecialUserType[];
   hasDelegates: boolean;
 
-  /**
-   * Date and time when the sender is created.
-   *
-   * @remarks
-   * Uses date-time notation as defined by {@link https://datatracker.ietf.org/doc/html/rfc3339#section-5.6 | RFC 3339, section 5.6}
-   */
-  created: string;
+  /** Date and time when the sender is created. */
+  created: DateTimeString;
 
-  /**
-   * Date and time when the sender is updated.
-   *
-   * @remarks
-   * Uses date-time notation as defined by {@link https://datatracker.ietf.org/doc/html/rfc3339#section-5.6 | RFC 3339, section 5.6}
-   */
-  updated: string;
+  /** Date and time when the sender is updated. */
+  updated: DateTimeString;
 
-  /**
-   * Date and time when the sender is locked.
-   *
-   * @remarks
-   * Uses date-time notation as defined by {@link https://datatracker.ietf.org/doc/html/rfc3339#section-5.6 | RFC 3339, section 5.6}
-   */
-  locked: Nullable<string>;
+  /** Date and time when the sender is locked. */
+  locked: Nullable<DateTimeString>;
 
-  /**
-   * Date and time when the sender is activated.
-   *
-   * @remarks
-   * Uses date-time notation as defined by {@link https://datatracker.ietf.org/doc/html/rfc3339#section-5.6 | RFC 3339, section 5.6}
-   */
-  activated: Nullable<string>;
+  /** Date and time when the sender is activated. */
+  activated: Nullable<DateTimeString>;
 }
