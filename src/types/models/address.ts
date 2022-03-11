5d81a0527ba7360aa9428fa902cb3b8108dd444c
SC Haw <haw@breathelife.com>
2022-03-11 13:43:41 -0500

    Typed address interface

--- src/types/models/address.ts
+++ src/types/models/address.ts
@@ -1,1 +1,15 @@
-export interface Address {}
+import { Nullable } from '../utils';
+
+/** @public */
+export interface Address {
+  state: Nullable<string>;
+  country: Nullable<string>;
+  city: Nullable<string>;
+  zipCode: Nullable<string>;
+
+  /** Address line 1 */
+  address1: Nullable<string>;
+
+  /** Address line 2 */
+  address2: Nullable<string>;
+}
