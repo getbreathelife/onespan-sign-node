773e2935cb0d7aeaba6156a302efe77cdc234c84
SC Haw <haw@breathelife.com>
2022-04-14 10:19:28 -0400

    Added callback resource class

---
+++ src/resources/CallbackResource.ts
@@ -0,0 +1,9 @@
+import { Resource } from './Resource';
+
+/**
+ * Resource class to interact with
+ * {@link https://community.onespan.com/documentation/onespan-sign/guides/feature-guides/developer/setting-callback-notifications | callbacks}.
+ *
+ * @public
+ */
+export class CallbackResource extends Resource {}
