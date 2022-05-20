19ff25a5d75de33b044bb759cd42769f193ca947
SC Haw <haw@breathelife.com>
2022-05-20 09:51:57 -0400

    Complete `Signer` model typing

--- src/types/models/group.ts
+++ src/types/models/group.ts
@@ -1,6 +1,38 @@
+import { CustomData, DateTimeString } from '../shared';
+import { Nullable } from '../utils';
+import Account from './account';
+
+/** @public */
+export type GroupMemberType = 'REGULAR' | 'MANAGER';
+
 /** @public */
 export interface GroupMembership {
   groupId: string;
   groupName: string;
-  memberType: 'REGULAR' | 'MANAGER';
+  memberType: GroupMemberType;
+}
+
+/** @public */
+export interface GroupMember {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  memberType: GroupMemberType;
+  pending: boolean;
+}
+
+/** @public */
+export interface Group {
+  id: string;
+  name: string;
+  members: GroupMember[];
+  emailMembers: boolean;
+  reciprocalDelegation: boolean;
+  created: DateTimeString;
+  updated: DateTimeString;
+
+  account: Nullable<Account>;
+  data: Nullable<CustomData>;
+  email: Nullable<string>;
 }
